6b4db24c7c5c7d6e4b7f98f045dcef96787680ec
Makarenkomd <mahadrum@yandex.ru>
2020-11-19 10:31:33 +0300

    Добавьте файлы проекта.

---
+++ TestGenius/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TestGenius
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNumberQuestion = new System.Windows.Forms.Label();
+            this.labelTextQuestion = new System.Windows.Forms.Label();
+            this.textBoxAnswerQuestion = new System.Windows.Forms.TextBox();
+            this.buttonNextQuestion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNumberQuestion
+            // 
+            this.labelNumberQuestion.AutoSize = true;
+            this.labelNumberQuestion.Location = new System.Drawing.Point(34, 32);
+            this.labelNumberQuestion.Name = "labelNumberQuestion";
+            this.labelNumberQuestion.Size = new System.Drawing.Size(67, 13);
+            this.labelNumberQuestion.TabIndex = 0;
+            this.labelNumberQuestion.Text = "Вопрос № 1";
+            // 
+            // labelTextQuestion
+            // 
+            this.labelTextQuestion.AutoSize = true;
+            this.labelTextQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTextQuestion.Location = new System.Drawing.Point(30, 64);
+            this.labelTextQuestion.Name = "labelTextQuestion";
+            this.labelTextQuestion.Size = new System.Drawing.Size(86, 31);
+            this.labelTextQuestion.TabIndex = 1;
+            this.labelTextQuestion.Text = "label2";
+            // 
+            // textBoxAnswerQuestion
+            // 
+            this.textBoxAnswerQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxAnswerQuestion.Location = new System.Drawing.Point(30, 174);
+            this.textBoxAnswerQuestion.Name = "textBoxAnswerQuestion";
+            this.textBoxAnswerQuestion.Size = new System.Drawing.Size(157, 38);
+            this.textBoxAnswerQuestion.TabIndex = 2;
+            // 
+            // buttonNextQuestion
+            // 
+            this.buttonNextQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonNextQuestion.Location = new System.Drawing.Point(190, 171);
+            this.buttonNextQuestion.Name = "buttonNextQuestion";
+            this.buttonNextQuestion.Size = new System.Drawing.Size(302, 41);
+            this.buttonNextQuestion.TabIndex = 3;
+            this.buttonNextQuestion.Text = "Следующий вопрос";
+            this.buttonNextQuestion.UseVisualStyleBackColor = true;
+            this.buttonNextQuestion.Click += new System.EventHandler(this.buttonNextQuestion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 296);
+            this.Controls.Add(this.buttonNextQuestion);
+            this.Controls.Add(this.textBoxAnswerQuestion);
+            this.Controls.Add(this.labelTextQuestion);
+            this.Controls.Add(this.labelNumberQuestion);
+            this.Name = "Form1";
+            this.Text = "тест ГЕНИЙ-ИДИОТ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNumberQuestion;
+        private System.Windows.Forms.Label labelTextQuestion;
+        private System.Windows.Forms.TextBox textBoxAnswerQuestion;
+        private System.Windows.Forms.Button buttonNextQuestion;
+    }
+}
+
---
+++ TestGenius/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestGenius
+{
+    public partial class Form1 : Form
+    {
+        string[] questions = {"2 + 2 * 2", 
+                        "Сколько распилов нужно, чтобы разделить бревно на 10 частей?", 
+                        "На 2-х руках 10 пальцев, сколько пальцев на 5 руках?",
+                        "Уколы делают каждые полчаса, через сколько минут сделают 3 укола?",
+                        "5 свечей горело, две погасло.Сколько свечей осталось?"};
+        int[] answers = { 6, 9, 25, 60, 2 };
+        string[] results = { "идиот", "дибил", "дурак", "норма", "талант", "гений" };
+        int rightAnswer = 0;
+
+        int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            labelTextQuestion.Text = questions[index];
+        }
+
+        private void buttonNextQuestion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int answer = Convert.ToInt32(textBoxAnswerQuestion.Text);
+                if (answer == answers[index])
+                    {
+                        MessageBox.Show("Верно!");
+                        rightAnswer++;
+                    }
+                else
+                    MessageBox.Show("Не верно");
+                if (index < questions.Length - 1)
+                    labelTextQuestion.Text = questions[++index];
+                else
+                    {
+                        MessageBox.Show(results[rightAnswer]);
+                    }
+            }
+            catch
+            {
+                MessageBox.Show("Введите число!");
+            }
+        }
+    }
+}
